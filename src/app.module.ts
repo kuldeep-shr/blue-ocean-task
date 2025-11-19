4ecd54187d88cec46d4085037d17f62c6ad94b83
kuldeep <kuldeepsharma8988@gmail.com>
2025-11-19 17:41:16 +0530

    project initiated 🔥

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
+import { CategoryModule } from './nest/category/category.module';
+
+@Module({
+  imports: [CategoryModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
